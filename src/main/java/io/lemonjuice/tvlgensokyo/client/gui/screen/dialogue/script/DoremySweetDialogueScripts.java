f84e0fb67b1154e92f6fe87af4bf9b15e722e2f9
LemonJuice95 <1582017385@qq.com>
2025-04-20 21:25:58 +0800

    Changed the method to change dimension

--- src/main/java/io/lemonjuice/tvlgensokyo/client/gui/screen/dialogue/script/DoremySweetDialogueScripts.java
+++ src/main/java/io/lemonjuice/tvlgensokyo/client/gui/screen/dialogue/script/DoremySweetDialogueScripts.java
@@ -5,6 +5,7 @@ import io.lemonjuice.tvlgensokyo.client.gui.screen.dialogue.DialogueButtonForScr
 import io.lemonjuice.tvlgensokyo.client.gui.screen.dialogue.DialoguePage;
 import io.lemonjuice.tvlgensokyo.client.gui.screen.dialogue.DialogueScript;
 import io.lemonjuice.tvlgensokyo.common.world.dimension.TGDimensionRegister;
+import io.lemonjuice.tvlgensokyo.utils.TGPlayerUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
@@ -19,19 +20,7 @@ public class DoremySweetDialogueScripts {
     public static DialogueScript getTempScript(PlayerEntity player, BlockPos destinyPosGensokyo) {
         DialoguePage page = new DialoguePage(new StringTextComponent("Not yet."));
         page.addButton(new DialogueButtonForScript(new TranslationTextComponent("dialogue.button.tvlgensokyo.to_the_gensokyo"), (button) -> {
-            if(player.world instanceof ServerWorld && player.canChangeDimension()) {
-                ServerWorld dimension = player.getServer().getWorld(TGDimensionRegister.GENSOKYO);
-                if(dimension != null) {
-                    player.changeDimension(dimension, new ITeleporter() {
-                        @Override
-                        public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
-                            entity = repositionEntity.apply(false);
-                            entity.teleportKeepLoaded(destinyPosGensokyo.getX(), destinyPosGensokyo.getY(), destinyPosGensokyo.getZ());
-                            return ITeleporter.super.placeEntity(entity, currentWorld, destWorld, yaw, repositionEntity);
-                        }
-                    });
-                }
-            }
+            TGPlayerUtils.changePlayerDimension(player, TGDimensionRegister.DREAM_WORLD, destinyPosGensokyo.getX(), destinyPosGensokyo.getY(), destinyPosGensokyo.getZ());
         }));
         DialogueScript script = new DialogueScript(ImmutableMap.of("tempPage", page), "tempPage");
         return script;
