41ef8d8843934300c2765f22aab7a0b3c07ecbe8
lemon_juice_1 <1582017385@qq.com>
2025-05-15 23:26:22 +0800

    添加樱花木

--- src/main/java/io/lemonjuice/tvlgensokyo/common/misc/TGWoodType.java
+++ src/main/java/io/lemonjuice/tvlgensokyo/common/misc/TGWoodType.java
@@ -6,4 +6,5 @@ import net.minecraft.util.ResourceLocation;
 
 public class TGWoodType {
     public static final WoodType MAPLE = WoodType.register(WoodType.create(new ResourceLocation(TravelogueGensokyo.MODID, "maple").toString()));
+    public static final WoodType SAKURA = WoodType.register(WoodType.create(new ResourceLocation(TravelogueGensokyo.MODID, "sakura").toString()));
 }
