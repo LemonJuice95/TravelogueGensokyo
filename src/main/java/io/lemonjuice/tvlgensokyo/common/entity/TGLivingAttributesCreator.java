73890456c9cef87ebe2db87dc01f67996a7e6937
lemon_juice_1 <1582017385@qq.com>
2025-06-10 00:53:12 +0800

    微量重构

--- src/main/java/io/lemonjuice/tvlgensokyo/common/entity/TGLivingAttributesCreator.java
+++ src/main/java/io/lemonjuice/tvlgensokyo/common/entity/TGLivingAttributesCreator.java
@@ -1,6 +1,6 @@
 package io.lemonjuice.tvlgensokyo.common.entity;
 
-import io.lemonjuice.tvlgensokyo.common.entity.creature.EntityKappa;
+import io.lemonjuice.tvlgensokyo.common.entity.creature.KappaEntity;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 public class TGLivingAttributesCreator {
     @SubscribeEvent
     public static void onAttributeCreate(EntityAttributeCreationEvent event) {
-        event.put(TGEntityRegister.KAPPA.get(), EntityKappa.createAttribute().create());
+        event.put(TGEntityRegister.KAPPA.get(), KappaEntity.createAttribute().create());
         event.put(TGEntityRegister.GENSOKYO_VILLAGER.get(), VillagerEntity.registerAttributes().create());
     }
 }
--- src/main/java/io/lemonjuice/tvlgensokyo/common/misc/CurrencyType.java
+++ src/main/java/io/lemonjuice/tvlgensokyo/common/misc/CurrencyType.java
@@ -1,15 +1,17 @@
 package io.lemonjuice.tvlgensokyo.common.misc;
 
-import com.google.common.collect.ImmutableMap;
 import io.lemonjuice.tvlgensokyo.common.item.TGItemRegister;
 import io.lemonjuice.tvlgensokyo.common.item.misc.CurrencyItem;
 
-import java.util.Map;
-
 public enum CurrencyType {
-    COPPER_COIN,
-    SILVER_PIECE,
-    SILVER_INGOT;
+    COPPER_COIN(TGItemRegister.COPPER_COIN.get()),
+    SILVER_PIECE(TGItemRegister.SILVER_PIECE.get()),
+    SILVER_INGOT(TGItemRegister.SILVER_INGOT.get());
 
-    public static final Map<CurrencyType, CurrencyItem> CURRENCY_MAP = ImmutableMap.of(COPPER_COIN, TGItemRegister.COPPER_COIN.get(), SILVER_PIECE, TGItemRegister.SILVER_PIECE.get(), SILVER_INGOT, TGItemRegister.SILVER_INGOT.get());
+    public final CurrencyItem currencyItem;
+
+    private CurrencyType(CurrencyItem currencyItem) {
+        this.currencyItem = currencyItem;
+    }
+
 }
